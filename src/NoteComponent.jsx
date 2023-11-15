e48ddf423ed572d7da4f91a24b2d7d6e89db59cb
Дарья Сендерихина <dawo.xorowo@gmail.com>
2023-11-16 02:51:10 +0300

    [MS4] done: active_inactive state for the Modal

--- src/NoteComponent.jsx
+++ src/NoteComponent.jsx
@@ -1,20 +1,29 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Modal from "./NoteModal.jsx";
+import "./NoteApp.css";
 
 const NoteComponent = ({ note, handleDelete }) => {
-    const handleNoteDelete = () => {
-        if (window.confirm("Are you sure you want to delete this sticker?")) {
-            handleDelete(note);
-        }
-    };
+  const [modalActive, setModalActive] = useState(false);
+
+  const handleNoteDelete = () => {
+    if (window.confirm("Are you sure you want to delete this sticker?")) {
+      handleDelete(note);
+    }
+  };
+
   return (
-    <div className='note'>
-        <button className="delete-button" onClick={handleNoteDelete}>X</button>
-    <p className="note-title">{note.title}</p>
-      <p className='note-text'>{note.text}</p>
-      <p className='note-date'>{note.createdDate}</p>
-      
-    </div>
+    <>
+      <Modal active={modalActive} setActive={setModalActive} note={note} />
+
+      <div className={`note ${modalActive ? 'inactive' : ''}`} onClick={() => setModalActive(true)}>
+        <button className="delete-button" onClick={handleNoteDelete}>
+          X
+        </button>
+        <p className="note-title">{note.title}</p>
+        <p className='note-text'>{note.text}</p>
+        <p className='note-date'>{note.createdDate}</p>
+      </div>
+    </>
   );
 };
 
