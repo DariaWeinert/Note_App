93a11df518ccdae6940bff8a37e2ab6a721feb09
Дарья Сендерихина <dawo.xorowo@gmail.com>
2023-11-16 03:51:12 +0300

    [MS5] editable Modal with save btn

--- src/NoteModal.jsx
+++ src/NoteModal.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./NoteModal.css";
+import "./NoteApp.css"
 
-const Modal = ({ active, setActive, note }) => {
+const Modal = ({ active, setActive, note, handleEditNote }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(note.title);
+  const [editedText, setEditedText] = useState(note.text);
+
+  useEffect(() => {
+    setIsEditing(true);
+  }, [active]);
+
+  const handleSave = () => {
+    handleEditNote(note, { title: editedTitle, text: editedText });
+    setIsEditing(false);
+  };
+
   return (
     <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <p className="note-title">{note.title}</p>
-        <p className='note-text'>{note.text}</p>
-        <p className='note-date'>{note.createdDate}</p>
+        <>
+          <input
+            className="title-input'"
+            type="text"
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+          />
+          <textarea
+            className='note-text'
+            value={editedText}
+            onChange={(e) => setEditedText(e.target.value)}
+          />
+          <p className='note-date'>{note.createdDate}</p>
+          <div className='btn-box'>
+          <button className="save-note-button" onClick={handleSave}>Save</button>
+          </div>
+        </>
       </div>
     </div>
   );
